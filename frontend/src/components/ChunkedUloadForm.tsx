48f2d7a9941cbdd52ef1e18b4b9f1021c4fbcc9d
redaelemawy <redaelemawy98@gmail.com>
2025-05-24 14:45:06 +0300

    feat: add pause upload

--- frontend/src/components/ChunkedUloadForm.tsx
+++ frontend/src/components/ChunkedUloadForm.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Form from './Form';
 const ChunkedUploadForm = () => {
   const [uploading, setUploading] = useState(false);
   const [uploadProgress, setUploadProgress] = useState(0);
-
+  const [chunkIndex, setChunkIndex] = useState(0);
+  const uploadStoppedRef = useRef(false);
+  // abort controller state
+  const [abortController, setAbortController] =
+    useState<AbortController | null>(null);
   const handleSubmit = async (file: File) => {
     if (!file) return;
 
@@ -34,6 +38,12 @@ const ChunkedUploadForm = () => {
       let chunkIndex = 0;
       if (sessionId && chunkSize) {
         for (let start = 0; start < file.size; start += chunkSize) {
+          if (uploadStoppedRef.current) {
+            console.log('uploadStopped', uploadStoppedRef.current);
+            return;
+          }
+          console.log('start', start);
+
           await uploadChunk(file, sessionId, start, chunkSize, chunkIndex);
           chunkIndex++;
           setUploadProgress(Math.round((start * 100) / file.size));
@@ -44,7 +54,10 @@ const ChunkedUploadForm = () => {
       setUploading(false);
     } catch (error) {
       console.error('Error during chunked upload:', error);
-      setUploading(false);
+      console.log('uploadStopped', uploadStoppedRef.current);
+      if (!uploadStoppedRef.current) {
+        setUploading(false);
+      }
     }
   };
 
@@ -60,7 +73,8 @@ const ChunkedUploadForm = () => {
         start + chunkSize
       } for session ${sessionId}`
     );
-
+    const abortController = new AbortController();
+    setAbortController(abortController);
     const end = Math.min(start + chunkSize, file.size);
     const chunkToUpload = file.slice(start, end);
 
@@ -73,10 +87,12 @@ const ChunkedUploadForm = () => {
       const res = await fetch('http://localhost:3000/api/upload/upload-chunk', {
         method: 'POST',
         body: formData,
+        signal: abortController.signal,
       });
 
       const data = await res.json();
       console.log('Chunk upload response:', data);
+      setChunkIndex(chunkIndex + 1);
       return data;
     } catch (error) {
       console.error('Chunk upload failed:', error);
@@ -98,14 +114,31 @@ const ChunkedUploadForm = () => {
     console.log('Upload complete:', data);
     return data;
   };
+  const stopUpload = () => {
+    uploadStoppedRef.current = true;
+    if (abortController) {
+      abortController.abort();
+    }
+  };
   return (
-    <Form
-      uploading={uploading}
-      uploadProgress={uploadProgress}
-      maxFileSize={10000000000} // 10GB
-      maxFileFormat={10000000000} // 10GB
-      onUpload={handleSubmit}
-    />
+    <>
+      {uploadStoppedRef.current && <div>Upload stopped</div>}
+      <Form
+        uploading={uploading}
+        uploadProgress={uploadProgress}
+        maxFileSize={10000000000} // 10GB
+        maxFileFormat={10000000000} // 10GB
+        onUpload={handleSubmit}
+      />
+      {uploading && (
+        <button
+          className="bg-accent text-white p-2  mt-4 rounded-md"
+          onClick={stopUpload}
+        >
+          Stop Upload
+        </button>
+      )}
+    </>
   );
 };
 
